8efda1c4a69d8e132bc4398c5ac36ada48a08575
Sarah Lim <sarah@sarahlim.com>
2016-11-25 14:57:56 -0600

    add better error handling for properties without SourceRanges or cssTexts

--- src/chrome/disableProperty.js
+++ src/chrome/disableProperty.js
@@ -9,7 +9,7 @@
  */
 export default async function disableProperty (
   instance: Object, cssStyle: CSSStyle, propName: string
-): Promise<() => Promise<CSSStyle>> {
+): Promise<() => Promise<CSSStyle>> | Promise<Error> {
   const { CSS }: { CSS: Object } = instance;
 
   // Get styleSheetId, cssText and style range
@@ -18,27 +18,49 @@ export default async function disableProperty (
     cssText: styleText,
     range: styleRange,
   } = cssStyle;
+
+  if (!styleText || !styleRange) {
+    return new Error('CSSStyle missing property cssText or range');
+  }
+
   // Get the range and text of the property to be disabled
   const styleProperties: CSSProperty[] = cssStyle.cssProperties;
   const prop: CSSProperty = styleProperties.find((p: CSSProperty) => p.name === propName);
-  const propText: string = prop.text;
+
+  /**
+   * Properties that are expansions of shorthand properties (e.g. `transition-duration`) will
+   * not have their own SourceRange or even text.
+   * Can't disable these independently, so throw an error,
+   * which will be handled in the upper scope.
+   */
+  if (!prop.text || !prop.range) {
+    return new Error(`Property ${prop.name} has no SourceRange`);
+  }
 
   // Construct the replacement text
-  const styleTextDisabled: string = styleText.replace(propText, `/* ${propText} */`);
+  const styleTextDisabled: string = styleText.replace(prop.text, `/* ${prop.text} */`);
 
-  // Disable the style, await the response
-  const response: Object = await CSS.setStyleTexts({
-    edits: [{
-      styleSheetId,
-      range: styleRange,
-      text: styleTextDisabled,
-    }],
-  });
+  let response: ?Object;
+  try {
+    response = await CSS.setStyleTexts({
+      edits: [{
+        styleSheetId,
+        range: styleRange,
+        text: styleTextDisabled,
+      }],
+    });
+  } catch (err) {
+    console.error(`Couldn't disable property ${propName}`, err);
+  }
 
   // Since the protocol returns a response object, need to destructure
   // twice to get the modified CSSStyle object
   const modifiedStyles: CSSStyle[] = response.styles;
   const modifiedStyle: CSSStyle = modifiedStyles[0];
+
+  if (!modifiedStyle.range) {
+    return new Error(`No SourceRange for CSSStyle returned after disabling property ${prop.name}`);
+  }
 
   // Get the modified range from the modified CSSStyle object
   const modifiedStyleRange: SourceRange = modifiedStyle.range;
@@ -50,7 +72,7 @@ export default async function disableProperty (
       range: modifiedStyleRange,
       text: styleText,
     }],
-  });
+  }).catch(err => console.error(err));
 
   return reenabler;
 }
